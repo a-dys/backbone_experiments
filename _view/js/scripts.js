9d9e5f581b1f97f089717c02a00d5d4ee911bd46
Ada Dys <adakokos@gmail.com>
2016-06-10 15:30:54 +0200

    Add class for person view

---
+++ _view/js/scripts.js
@@ -0,0 +1,39 @@
+var Person = Backbone.Model.extend({
+
+});
+
+var People = Backbone.Collection.extend({
+});
+
+var PersonView = Backbone.view.extend({
+    tagName: 'li',
+    template: _.template($("#personTemplate".html())),
+    render: function () {
+        var html = this.template(this.model.toJSON());
+        return this;
+    }
+
+});
+
+var person1 = new Person({
+    id: 1,
+    name: "John",
+    age: 33,
+    hobbies: ["sport", "IT"]
+});
+
+var person2 = new Person({
+    id: 2,
+    name: "Thomas",
+    age: 40,
+    hobbies: ["golf", "cars", "movies"]
+});
+
+var person3 = new Person({
+    id: 3,
+    name: "Alice",
+    age: 25,
+    hobbies: ["swimming"]
+});
+
+var people = new People([person1, person2, person3]);
