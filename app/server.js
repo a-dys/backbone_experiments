795df791bc884b6a187cf5fde09855a4bef6ade6
Ada Dys <adakokos@gmail.com>
2016-06-18 12:09:37 +0200

    Configure primary path

--- app/server.js
+++ app/server.js
@@ -1,6 +1,10 @@
 var express = require("express"),
     app = express();
 
+app.get("/", function (req, res) {
+    res.send("Hello my dear user!");
+});
+
 app.listen("8000", function () {
     console.log("Server is running!");
 });
